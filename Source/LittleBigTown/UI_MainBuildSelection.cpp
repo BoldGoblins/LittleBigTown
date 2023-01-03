81139f1e5ef6ce7d9ac9dcbd8c6720f670afdaa4
BoldGoblins <111541822+BoldGoblins@users.noreply.github.com>
2023-01-03 23:12:43 +0100

    Class not in use deletion
    Class not in use deletion

--- Source/LittleBigTown/UI_MainBuildSelection.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "UI_MainBuildSelection.h"
-#include "UIBuildingButton.h"
-#include "Components/ScrollBox.h"
-
-
-// --------------------------------------- CLASS NOT IN USE --------------------------------------- 
